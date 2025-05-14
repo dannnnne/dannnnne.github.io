7f5145c31e0e05abfb0f667590aa01f766c99f61
daeun <rlaekdms901@naver.com>
2025-05-14 22:37:16 +0900

    [ FEAT ] STACKS 페이지 UI 틀 잡기

--- portfolio/src/components/stacks.jsx
+++ portfolio/src/components/stacks.jsx
@@ -1,5 +1,13 @@
 const Stacks = () => {
-  return <div className="bg-[rgb(241,239,236)] h-[1300px]">stacks</div>;
+  return (
+    <div className="bg-[rgb(241,239,236)] h-[1024px] ml-[20%]">
+      <div className="">
+        <h1 className="sm:text-[25px] md:text-[30px] xl:text-[40px] mx-5 2xl:ml-10 font-black xl:h-[60px] pt-5">
+          STACKS
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Stacks;
