71ed91b2ffd67ab235af8234eaa3f88118160a3e
daeun <rlaekdms901@naver.com>
2025-05-15 23:14:30 +0900

    [ FEAT ] 각 폴더로 분리

--- portfolio/src/components/projectDetail.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-const ProjectDetail = () => {
-  const params = useParams();
-  return <div>{params.id}번 프로젝트 입니다.</div>;
-};
-
-export default ProjectDetail;
