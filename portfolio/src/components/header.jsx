e0e1a38e30a79dd3782e5b8bdea0df9eba2c198f
daeun <rlaekdms901@naver.com>
2025-05-19 21:58:29 +0900

    [ FEAT ] header 애니메이션 구현, 링크 연결

--- portfolio/src/components/header.jsx
+++ portfolio/src/components/header.jsx
@@ -3,14 +3,20 @@ import VelogImg from '../assets/velog.jpg';
 import GmailImg from '../assets/gmail.png';
 import profileImg from '../assets/hi.jpg';
 
+import { useState } from 'react';
+
 const Header = () => {
+  const [hoverd, setHovered] = useState(false);
+
   return (
     <div className="bg-[rgb(27,38,59)] w-[20%] h-screen fixed  left-0 top-[0px] flex flex-col justify-between py-5">
       <div className="mt-16 flex flex-col justify-center items-center -mb-14 ">
-        <img src={profileImg} className="w-1/2 mx-auto mb-5 rounded-full" />
-        <h2 className="text-white sm:text-xl lg:text-2xl text-sm">
+        <a href="#Hello">
+          <img src={profileImg} className="w-1/2 mx-auto mb-5 rounded-full" />
+        </a>
+        <a className="text-white sm:text-xl lg:text-2xl text-sm" href="#Hello">
           DA EUN KIM
-        </h2>
+        </a>
         <p className="text-[rgb(153,153,153)] sm:text-[12px] lg:text-lg text-[9px]">
           Front-End Developer
         </p>
@@ -18,24 +24,51 @@ const Header = () => {
 
       <div>
         <ul className="text-white flex flex-col justify-center items-center gap-6 text-lg sm:text-2xl">
-          <li>ABOUT</li>
-          <li>STACKS</li>
-          <li>PROJECTS</li>
+          <li className="transition-transform duration-300 hover:scale-110">
+            <a href="#About">ABOUT</a>
+          </li>
+          <li className="transition-transform duration-300 hover:scale-110">
+            <a href="#Stacks">STACKS</a>
+          </li>
+
+          <li className="transition-transform duration-300 hover:scale-110 ">
+            <a href="#Projects">PROJECTS</a>
+          </li>
         </ul>
       </div>
 
       <div className="flex justify-center gap-2 sm:gap-5">
-        <div className="bg-white rounded-[50%] w-6 h-6 sm:w-9 sm:h-9">
+        <a
+          className="bg-white rounded-[50%] w-6 h-6 sm:w-9 sm:h-9"
+          href="https://github.com/dannnnne"
+          target="_blank"
+        >
           <img src={githubImg} className=" w-6 h-6 sm:w-11 sm:h-9" />
-        </div>
-        <div className="w-6 h-6 sm:w-9 sm:h-9 bg-white rounded-[50%]">
+        </a>
+        <a
+          className="w-6 h-6 sm:w-9 sm:h-9 bg-white rounded-[50%]"
+          href="https://velog.io/@da_eun_e/posts"
+          target="_blank"
+        >
           <img src={VelogImg} className="w-6 h-6 sm:w-9 sm:h-9 rounded-[50%]" />
-        </div>
-        <div className="bg-white rounded-[50%] w-6 h-6 sm:w-9 sm:h-9 relative">
+        </a>
+        <div
+          className="bg-white rounded-[50%] w-6 h-6 sm:w-9 sm:h-9 relative"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
           <img
             src={GmailImg}
-            className="absolute w-5 h-5 right-[2px] top-[2px]  sm:w-8 sm:h-8"
+            className="absolute w-5 h-5 right-[2px] top-[2px] sm:w-8 sm:h-8"
           />
+          {hoverd && (
+            <div className="w-[290px] h-[70px] text-center py-2 absolute bottom-[55px] left-[-50px] md:left-[-130px] lg:left-[-160px] rounded-lg bg-white border border-[rgb(27,38,59)] lg:w-[290px]">
+              <span className="text-green-600">naver</span> :
+              rlaekdms901@naver.com
+              <br />
+              <span className="text-red-800">khu</span> : rlaekdms901@khu.ac.kr
+            </div>
+          )}
         </div>
       </div>
     </div>
