30797ad8aed7dabac1a9aa3966410a3455e55f6b
daeun <rlaekdms901@naver.com>
2025-05-20 19:36:23 +0900

    [ STYLE ] Hello 컴포넌트 애니메이션 추가

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -5,6 +5,15 @@ export default {
       fontFamily: {
         bmjua: ['BMJUA', 'sans-serif'], // ✅ 커스텀 폰트 등록
       },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 2s ease-in-out forwards',
+      },
     },
   },
   plugins: [],
