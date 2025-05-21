35a3d1f6842735b2193a024c5be9721e5fe63d38
daeun <rlaekdms901@naver.com>
2025-05-21 23:03:05 +0900

    [ FEAT ] 클릭 시 해당 내용 뜨게 구현

--- portfolio/src/components/project/projectDetail.jsx
+++ portfolio/src/components/project/projectDetail.jsx
@@ -1,19 +1,30 @@
 import { useParams } from 'react-router-dom';
 import Arrow from '../../assets/arrow.png';
+import { projectData } from '../../constant/projectData';
 
 const ProjectDetail = () => {
   const { id } = useParams();
+  const decodedTitle = decodeURIComponent(id);
+  const project = projectData[decodedTitle];
   // {params.id}번 프로젝트 입니다.
+
+  if (!project) {
+    return <div>해당 프로젝트를 찾을 수 없습니다.</div>;
+  }
   return (
     <div className="bg-[rgb(241,239,236)] ">
-      <div className="w-[80vw] sm:w-[600px] xl:w-[800px] bg-[rgb(27,38,59)] h-[90vh] mx-auto my-[3%] rounded-xl shadow-2xl flex flex-col justify-between border border-none">
+      <div className="w-[80vw] sm:w-[600px] xl:w-[800px] bg-[rgb(93,122,175)] h-[90vh] mx-auto my-[3%] rounded-xl shadow-2xl flex flex-col justify-between border border-none">
         <div className="border-b-2 border-[rgb(177,177,177)] h-20 flex px-3 items-center">
           <img src={Arrow} className="w-10 h-10" />
-          <div className="text-center flex-1 text-white ">제목</div>
+          <div className="text-center flex-1 text-white text-xl ">
+            {decodedTitle}
+          </div>
         </div>
-        <div className="border-b-2 flex-1 border-[rgb(177,177,177)]">내용</div>
+        <div className="border-b-2 flex-1 border-[rgb(177,177,177)]">
+          <img className="w-full h-[370px] mx-auto" src={project.img} />
+        </div>
         <div className="text-black h-[45%] outline-none border-none bg-white rounded-b-xl">
-          설명
+          {project.description}
         </div>
       </div>
     </div>
