6747511e50ed144e1d6e6dbee054a8d808d09ae4
daeun <rlaekdms901@naver.com>
2025-05-21 22:19:34 +0900

    [ FEAT ] navigate 추가

--- portfolio/src/components/project/projects.jsx
+++ portfolio/src/components/project/projects.jsx
@@ -1,6 +1,17 @@
 import { motion } from 'framer-motion';
+import ProjectGrid from './projectGrid';
+
+import ArtSW from '../../assets/projects/예소.jpg';
+import GroomUniv from '../../assets/projects/구름톤유니브.png';
+import HomePage from '../../assets/projects/homepage.png';
+import { useNavigate } from 'react-router-dom';
 
 const Projects = () => {
+  const navigate = useNavigate();
+  const onClick = (title) => {
+    navigate(`/project/${title}`);
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 50 }}
@@ -15,19 +26,32 @@ const Projects = () => {
       </h1>
 
       <div className="grid lg:grid-cols-2 2xl:grid-cols-3 gap-5 mx-auto md:mx-5 2xl:mx-10 mt-10 mb-10">
-        <div className="mx-auto w-[300px] h-[270px] sm:w-[350px] sm:h-[320px]  md:w-[380px] md:h-[350px] xl:w-[430px] xl:h-[400px] 2xl:w-[95%] 2xl:h-[400px] bg-[rgb(216,217,218)] flex flex-col justify-end rounded-[20px] ">
-          <img src="../../assets/projects/예소.jpg" alt="" />
-          <p className="bg-white h-[30%]">설명</p>
-        </div>
-        <div className="mx-auto w-[300px] h-[270px] sm:w-[350px] sm:h-[320px]  md:w-[380px] md:h-[350px] xl:w-[430px] xl:h-[400px] 2xl:w-[95%] 2xl:h-[400px] bg-[rgb(216,217,218)] flex flex-col justify-end rounded-[20px] ">
-          <p className="bg-white h-[30%]">설명</p>
-        </div>{' '}
-        <div className="mx-auto w-[300px] h-[270px] sm:w-[350px] sm:h-[320px]  md:w-[380px] md:h-[350px] xl:w-[430px] xl:h-[400px] 2xl:w-[95%] 2xl:h-[400px] bg-[rgb(216,217,218)] flex flex-col justify-end rounded-[20px] ">
-          <p className="bg-white h-[30%]">설명</p>
-        </div>{' '}
-        <div className="mx-auto w-[300px] h-[270px] sm:w-[350px] sm:h-[320px]  md:w-[380px] md:h-[350px] xl:w-[430px] xl:h-[400px] 2xl:w-[95%] 2xl:h-[400px] bg-[rgb(216,217,218)] flex flex-col justify-end rounded-[20px] ">
-          <p className="bg-white h-[30%]">설명</p>
-        </div>
+        <ProjectGrid
+          img={HomePage}
+          title={'스터디 홈페이지 만들기'}
+          description={'설명'}
+          time={'2025.04.02 ~ ing'}
+          stacks={['React', 'TS', 'NextJS', 'tailwindCSS']}
+          onClick={() => onClick('스터디_홈페이지')}
+        />
+        <ProjectGrid
+          img={ArtSW}
+          title={'예술적인 소프트웨어'}
+          description={'설명'}
+          time={'2025.05.11 ~ ing'}
+          stacks={['React', 'JS', 'TS', 'tailwindCSS']}
+          onClick={() => {
+            onClick('예술적인_소프트웨어');
+          }}
+        />
+        <ProjectGrid
+          img={GroomUniv}
+          title={'구름톤 유니브 교내 프로젝트'}
+          description={'설명'}
+          time={'2025.05.21 ~ ing'}
+          stacks={['React', 'JS', 'TS', 'tailwindCSS']}
+          onClick={() => onClick('구름톤_유니브_교내_프로젝트')}
+        />
       </div>
     </motion.div>
   );
