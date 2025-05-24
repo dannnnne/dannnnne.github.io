38bd0112aec680fee91bc345681f1464c0c027c4
daeun <rlaekdms901@naver.com>
2025-05-24 22:06:06 +0900

    github 배포

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/dannnnne.github.io/', // ✅ 이건 그대로 둬야 GitHub Pages에서 JS 로드됨
+  base: '/', // ✅ 이거로 바꿔!
   plugins: [react()],
 });
